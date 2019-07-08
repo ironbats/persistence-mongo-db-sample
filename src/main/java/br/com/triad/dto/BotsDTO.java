97fe740fec590d5ea8e3881705265e86b2c43f83
Felipe Rodrigues <felipe.rodrigues@keyrus.com.br>
2019-07-08 01:28:19 -0300

    implements   groovy test

--- src/main/java/br/com/triad/dto/BotsDTO.java
+++ src/main/java/br/com/triad/dto/BotsDTO.java
@@ -1,8 +1,14 @@
 package br.com.triad.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Wither;
 
 @Data
+@Wither
+@NoArgsConstructor
+@AllArgsConstructor
 public class BotsDTO {
 
     private String id;
---
+++ src/test/java/br/com/triad/MessagesFixture.groovy
@@ -0,0 +1,15 @@
+package br.com.triad
+
+import br.com.triad.dto.BotsDTO
+
+    class MessagesFixture {
+
+
+        static def buildAMessage()
+        {
+            return new BotsDTO(id: "5d22bfe9d5235f1fae19f01c",name: "bots_name")
+        }
+    }
+
+
+
