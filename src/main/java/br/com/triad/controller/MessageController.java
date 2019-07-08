09dbd638ca25e5bf3c7c25474cfc3a946fcf0d03
Felipe Rodrigues <felipe.rodrigues@keyrus.com.br>
2019-07-08 04:42:00 -0300

    implements cache

--- src/main/java/br/com/triad/controller/MessageController.java
+++ src/main/java/br/com/triad/controller/MessageController.java
@@ -8,6 +8,7 @@ import com.wordnik.swagger.annotations.ApiOperation;
 import com.wordnik.swagger.annotations.ApiResponse;
 import com.wordnik.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -35,6 +36,7 @@ public class MessageController {
     }
 
     @GetMapping(value = "/{id}")
+    @Cacheable("messages")
     @ResponseBody
     @ApiOperation("get specific messages for id")
     @ApiResponses(value = {
@@ -52,6 +54,7 @@ public class MessageController {
     @GetMapping
     @ResponseBody
     @ApiOperation("return all conversations  by conversationId")
+    @Cacheable("conversations")
     public String getConversation(@RequestParam(value = "conversationId",defaultValue = "false")String conversationId) throws IOException
     {
         ObjectMapper objectMapper = new ObjectMapper();
